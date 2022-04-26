93790987bcc71144b10d9ff4cb2db97da86325a9
Your Name <you@example.com>
2022-04-26 15:29:46 +0600

    initial 2

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -65,7 +65,7 @@ const Shop = () => {
               className={page === number ? "selected" : ""}
               onClick={() => setPage(number)}
             >
-              {number}
+              {number + 1}
             </button>
           ))}
 
